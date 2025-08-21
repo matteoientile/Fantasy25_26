e2e67509e0ab8ff2fccad003e926b6e135ef7e73
Matteo Ientile <158523435+matteoientile@users.noreply.github.com>
2025-08-21 17:13:47 +0200

    Update Fantacalcio25_26_Application.py

--- Fantacalcio25_26_Application.py
+++ Fantacalcio25_26_Application.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+import matplotlib.pyplot as plt
+from plotly.subplots import make_subplots
+from dash import Dash, dcc, html, Input, Output
 
