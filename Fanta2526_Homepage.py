82f62726851b18821d937ba27a232123cf483592
Matteo Ientile <158523435+matteoientile@users.noreply.github.com>
2025-08-22 15:38:07 +0200

    Update Fanta2526_Homepage.py

--- Fanta2526_Homepage.py
+++ Fanta2526_Homepage.py
@@ -8,11 +8,11 @@ from plotly.subplots import make_subplots
 from dash import Dash, dcc, html, Input, Output
 
 st.set_page_config(
-    page_title="Hello",
+    page_title="Fantacalcio 25/26 - Asta Statistica",
     page_icon="👋",
 )
 
-st.write("# Fantacalcio 25/26 - Tutte le statistiche")
+st.write("# Fantacalcio 25/26 - Asta Statistica")
 
 st.sidebar.success("Select a demo above.")
 
