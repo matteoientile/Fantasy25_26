435309d1d0dbcd8e09583f61b034df753c63a9fe
Matteo Ientile <158523435+matteoientile@users.noreply.github.com>
2025-08-26 17:16:40 +0200

    Update Fanta2526_Homepage.py

--- Fanta2526_Homepage.py
+++ Fanta2526_Homepage.py
@@ -1,3 +1,24 @@
+import streamlit as st
+st.set_page_config(page_title="Fantacalcio 25/26", layout="wide")
+
+st.title("📊 Fantacalcio 25/26 - Asta Statistica")
+
+st.markdown(""" È il momento di **svoltare** la tua asta!
+
+Smettila di scorrere listoni interminabili, consumare decine di video pieni di bias ed opinioni personali: **qui parlano i numeri**.
+
+### 🔎 Cosa troverai:
+Tantissime statistice (Calcistiche e Fantacalcistiche) su 
+- 🧤 **Portieri** 
+- 🛡️ **Difensori** 
+- 📐 **Centrocampisti** 
+- 🎯 **Attaccanti**
+
+delle **ultime 3 stagioni di Serie A**.
+
+**❓ Non capisci nulla di statistica ❓** ➡️ Nessun problema! Ecco come leggere i grafici: """)
+
+
 with st.expander("🔢 SPIEGAZIONE SEMPLICE (anche se odi la statistica)"):
     st.markdown("""
     ### 🔗 Matrice di Correlazione
