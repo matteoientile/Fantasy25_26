eab86d1e644d45f177fb86297145e444e75c7b31
Matteo Ientile <158523435+matteoientile@users.noreply.github.com>
2025-08-27 15:46:30 +0200

    Update 5_Fanta2526_Listone&Formazioni.py

--- pages/5_Fanta2526_Listone&Formazioni.py
+++ pages/5_Fanta2526_Listone&Formazioni.py
@@ -4,8 +4,8 @@ st.set_page_config(page_title="Fantacalcio 25/26 - Listone & Probabili Formazion
 st.title("📋 Listone Stagione 25/26 & ⚽ Probabili Formazioni ")
 st.markdown("""
 Qui troverai 
-- i Link ai migliori siti per le Probabili Formazioni di Serie A
 - il Listone per l'asta 25/26
+- i Link ai migliori siti per le Probabili Formazioni di Serie A
 """)
 #========================= SIDEBAR: INDICE =========================
 st.sidebar.header("📌 Indice")
