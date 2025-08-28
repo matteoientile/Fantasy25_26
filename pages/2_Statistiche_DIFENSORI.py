0204350bbbf15878acd63a3f519a7f00b7a94bc3
Matteo Ientile <158523435+matteoientile@users.noreply.github.com>
2025-08-28 22:42:50 +0200

    Update 2_Statistiche_DIFENSORI.py

--- pages/2_Statistiche_DIFENSORI.py
+++ pages/2_Statistiche_DIFENSORI.py
@@ -59,9 +59,6 @@ def add_metrics(df, season_label=None):
     # %
     df["% Gol/Tiri"] = df["Gf"] / df["shots"].replace({0: np.nan})
     
-    # stats_per90
-    df["Amm a partita"] = df["Amm"] / df["Pv"].replace({0: np.nan})
-    
     # Season label
     if season_label:
         df["season"] = season_label
@@ -148,7 +145,7 @@ st.plotly_chart(fig, use_container_width=True)
 #========================= SECTION 1: BOX PLOTS =========================
 st.header("📊 Boxplot Difensori")
 metrics = ["Mv","Fm","Gf","Ass", "Efficienza realizzativa (Gol)", "Efficienza realizzativa (Assist)","xG_per90","xA_per90", "key_passes","G + A (pts converted)",
-           "Rc","R+","Amm","Amm a partita"]
+           "Rc","R+","Amm"]
 
 def add_boxplot(fig, df, col, metric):
     box = px.violin(df, y=metric, box=True, points="all", hover_data=["Nome","Squadra","Pv"])
